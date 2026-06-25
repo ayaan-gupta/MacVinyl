e0c136e6ef55497d9ea47987d4109c2dc6501fcb
Ayaan Gupta <aayugupta2009@gmail.com>
2026-06-25 18:29:34 -0500

    Initial Commit

---
+++ Vinyl/VinylApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinylApp.swift
+//  Vinyl
+//
+//  Created by Ayaan Gupta on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct VinylApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
